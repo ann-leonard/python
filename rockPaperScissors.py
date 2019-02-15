572f0b13bc2ebbf3f573e7c7d04bacf75d2a7118
ann-leonard <aleo0779@gmail.com>
2019-02-15 13:01:54 -0500

    error

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,12 +1,26 @@
 import random
-moves = ['rock','paper','scissors']
-playerMove = input("Please choose 'rock', 'paper', or 'scissors': ")
 
-compMove = random.choice(moves)
-print(f'The computer chose {compMove}.')
-if compMove == playerMove:
-    print("It's a tie")
-elif compMove == 'rock' and playerMove == 'paper' or compMove == 'paper' and playerMove == 'scissors' or compMove == 'scissors' and playerMove == 'rock':
-    print("you win!")
-else:
-    print("You lose!")
+playerScore = 0
+compScore = 0
+
+def game():
+
+    moves = ['rock','paper','scissors']
+    playerMove = input("Please choose 'rock', 'paper', or 'scissors': ")
+
+    compMove = random.choice(moves)
+    print(f'The computer chose {compMove}.')
+
+
+    if compMove == playerMove:
+        print("It's a tie- no points")
+    elif compMove == 'rock' and playerMove == 'paper' or compMove == 'paper' and playerMove == 'scissors' or compMove == 'scissors' and playerMove == 'rock':
+        print("you win!")
+        playerScore +=1
+    else:
+        print("You lose!")
+        compScore +=1
+
+    print(score)
+
+game()
